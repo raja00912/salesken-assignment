83e46057202bb008251899567ec1e89d39042697
raja00912cd <mdramiz025@gmail.com>
2023-02-18 21:36:49 +0530

    added email validation in login and signup page

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+document.getElementById("loginBtn").addEventListener("click", () => {
+    event.preventDefault();
+    let users = JSON.parse(localStorage.getItem("userdata")) || []
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+
+    let emailcheck = false;
+    let passcheck = false;
+    let username = "";
+
+    users.forEach((elem) => {
+        if (elem.email == email) {
+            username = elem.name
+            emailcheck = true;
+        }
+        if (elem.password == password) {
+            passcheck = true;
+        }
+    })
+
+    if (emailcheck) {
+        if (passcheck) {
+            localStorage.setItem("currentuser", username)
+            location.href = "quiz.html"
+        }
+        else {
+            alert("Incorrect password");
+        }
+    }
+    else {
+        alert("Incorrect email");
+    }
+
+})
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,40 @@
 document.getElementById("loginBtn").addEventListener("click", () => {
+    event.preventDefault();
+    let users = JSON.parse(localStorage.getItem("userdata")) || []
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    let emailcheck = false
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        emailcheck = true
+    }
+    else {
+        alert("Enter valid email")
+    }
+
+    for (let i = 0; i < users.length; i++) {
+        if (email == users[i].email) {
+            emailcheck = false
+            alert("Account already exists");
+            break;
+        }
+    }
+
+
+    if (name && emailcheck && password) {
+        let userdata = {
+            name,
+            email,
+            password
+        }
+        users.push(userdata)
+        console.log(users)
+        localStorage.setItem("userdata", JSON.stringify(users))
+        alert("Account created successfully. Redirecting to login page")
+        setTimeout(() => {
+            location.href = "index.html"
+        }, 1000)
+    }
 
 })
